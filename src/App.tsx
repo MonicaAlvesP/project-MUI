238dd0e5783f2b02ec92ad641a976959933facd2
MonicaAlvesP <monica52782@gmail.com>
2024-08-06 23:30:05 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { AppRoutes } from "./routes"
+
+
 export const App = () => {
   return (
-    <div>
-      <h2>Hello pessoa que cria um projeto npm e não instala o npm, você está precisando rever seus conceitos!!</h2>
-      <b>a seguir, uma comida de rabo do Copilot...</b>
-      <p>Sério, é muito importante instalar o npm para gerenciar as dependências do seu projeto. Sem o npm, você não poderá instalar pacotes externos, atualizar dependências ou executar scripts personalizados. Certifique-se de executar o comando "npm install" no diretório raiz do seu projeto para instalar todas as dependências necessárias.</p>
-    </div>
+    <>
+      <AppRoutes />
+    </>
   )
 }
 

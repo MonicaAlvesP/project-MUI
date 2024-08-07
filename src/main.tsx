3a1fec3207212cb717cff45ea533d56de90e2620
MonicaAlvesP <monica52782@gmail.com>
2024-08-07 00:15:48 -0300

    feat: Adicionando ThemeProvider do MUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import { ThemeProvider } from '@mui/material'
+import { LightTheme } from './shared/theme/Light.ts'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path='/pagina-inicial' element={<Button>Pagina Inicial</Button>} />
+      <Route path='/pagina-inicial' element={<Button variant='contained' color='primary'>Pagina Inicial</Button>} />
 
       <Route path='*' element={<Navigate to="/pagina-inicial" />} />
     </Routes>
